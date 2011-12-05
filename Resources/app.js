6cfac55956b15da599816d28bce913dc1afb9d44
Srinivasan Raguraman <ThoughtWorks@blankslate-sr5977.local>
2011-12-05 15:15:57 -0800

    SR/RT - adding basic layout to get values on the screen for the currency converter.

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,35 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+Titanium.UI.setBackgroundColor('#fff');
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
 
 
 //
 // create base UI tab and root window
 //
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+var win = Titanium.UI.createWindow({  
+    title:'Currency Converter',
+    backgroundColor:'#fff',
+    layout: 'vertical'
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
+ 
+win.add(Ti.UI.createLabel({
+	text : 'Currency Converter',
+	color : 'black',
+	width : 'auto',
+	height : 'auto'
+}));
 
 
+win.add(Ti.UI.createTextField({
+	hintText : 'Enter money value in USD',
+	height : '50'
+}));
 
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
+win.add(Ti.UI.createButton({
+	title: "Convert",
+	backgroundColor: "#794289"
+}));
 
 
-// open tab group
-tabGroup.open();
+win.open();
