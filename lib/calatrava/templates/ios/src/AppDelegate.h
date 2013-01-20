dcb90e666ea2db7265d287edf2c3643a450cde0d
Giles Alexander <giles.alexander@gmail.com>
2013-01-20 20:57:34 +0000

    Default handling of ajax requests: show and hide the network activity indicator.

--- lib/calatrava/templates/ios/src/AppDelegate.h
+++ lib/calatrava/templates/ios/src/AppDelegate.h
@@ -1,6 +1,11 @@
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+#import "CalatravaAppDelegate.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, CalatravaAppDelegate>
+{
+  int outstandingAjaxRequests;
+}
 
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) UINavigationController *rootNavController;
