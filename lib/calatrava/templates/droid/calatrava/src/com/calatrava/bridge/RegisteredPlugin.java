9681e37eaae29f73556a63cb317a12790e9e07e9
Giles Alexander <giles.alexander@gmail.com>
2013-02-03 18:38:31 +0000

    Other people will want to implement plugins.

--- lib/calatrava/templates/droid/calatrava/src/com/calatrava/bridge/RegisteredPlugin.java
+++ lib/calatrava/templates/droid/calatrava/src/com/calatrava/bridge/RegisteredPlugin.java
@@ -3,7 +3,7 @@ package com.calatrava.bridge;
 import android.content.Context;
 import java.util.Map;
 
-interface RegisteredPlugin
+public interface RegisteredPlugin
 {
   void setContext(PluginRegistry registry, Context ctxt);
   void call(String method, Map<String, Object> args);
