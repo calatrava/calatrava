95775778f15737657977a45c2def977274e3f9d5
Christoph Burgmer <cburgmer@ira.uka.de>
2013-02-23 10:55:44 +0000

    Fail if coffee script cannot be compiled, rather than quietly moving on to the next step

--- lib/calatrava/tasks/assets.rb
+++ lib/calatrava/tasks/assets.rb
@@ -5,6 +5,7 @@ end
 def coffee(in_dir_or_file, out_dir)
   if !Dir["#{in_dir_or_file}/**/*.coffee"].empty? || File.exists?(in_dir_or_file)
     $stdout.puts "coffee #{in_dir_or_file} -> #{out_dir}"
-    system "node_modules/coffee-script/bin/coffee --compile --output #{out_dir} #{in_dir_or_file}"
+    ok = system "node_modules/coffee-script/bin/coffee --compile --output #{out_dir} #{in_dir_or_file}"
+    fail "Error compiling coffee script. Did you run `npm install`?" if !ok
   end
 end
