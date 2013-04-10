00903e4d5de34d476c44aac3d99d5fdd4cce81a1
Giles Alexander <giles.alexander@gmail.com>
2013-04-10 17:32:49 -0500

    Include the offending path in the error.

--- lib/calatrava/tasks/assets.rb
+++ lib/calatrava/tasks/assets.rb
@@ -6,6 +6,6 @@ def coffee(in_dir_or_file, out_dir)
   if !Dir["#{in_dir_or_file}/**/*.coffee"].empty? || File.exists?(in_dir_or_file)
     $stdout.puts "coffee #{in_dir_or_file} -> #{out_dir}"
     ok = system "node_modules/coffee-script/bin/coffee --compile --output #{out_dir} #{in_dir_or_file}"
-    fail "Error compiling coffee script. Did you run `npm install`?" if !ok
+    fail "Error compiling CoffeeScript: '#{in_dir_or_file}'" if !ok
   end
 end
