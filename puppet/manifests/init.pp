d511159a10ba46e3e0404a7c72fdf4972eaac1b2
Vivek Jain <vivekjain10@gmail.com>
2013-08-04 08:35:07 -0400

    Installing all dependencies on vagrant instance to ease development/testing

---
+++ puppet/manifests/init.pp
@@ -0,0 +1,49 @@
+
+stage { 'req-install': before => Stage['rvm-install'] }
+
+class requirements {
+  exec { "apt-update":
+    command => "/usr/bin/apt-get -y update"
+  }
+  include java
+  package { ["ant", "apache2"]:
+    ensure => installed,
+    require => Exec['apt-update']
+  }
+}
+
+class installrvm {
+  include rvm
+  rvm::system_user { vagrant: ; }
+
+  rvm_system_ruby {
+    'ruby-1.9.3':
+    ensure => 'present',
+    default_use => true;
+  }
+}
+
+class installandroid {
+  class {'android': user => 'vagrant'}
+
+  android::platform { 'android-17': }
+
+  file {"/home/vagrant/.bash_profile":
+    owner   => vagrant,
+    group   => vagrant,
+    mode    => 0644,
+    content => template("/vagrant/puppet/templates/.bash_profile"),
+    require => Class['android'],
+  }
+}
+
+class doinstall {
+  class { requirements:
+    stage => "req-install"
+  }
+  include installrvm
+  include nodejs
+  include installandroid
+}
+
+include doinstall
---
+++ puppet/templates/.bash_profile
@@ -0,0 +1,2 @@
+export ANDROID_HOME=/usr/local/android/android-sdk-linux
+export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
